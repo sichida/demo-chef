39c7b1724db72630f3a99944c0ba8d3591a76631
Shoun Ichida <s.ichida@novediagroup.com>
2014-11-04 00:16:39 +0100

    Configuring repo for demo

--- cookbooks/nginx/recipes/balancing.rb
+++ cookbooks/nginx/recipes/balancing.rb
@@ -4,7 +4,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default_conf 			= [node[:nginx][:conf_dir],node[:nginx][:sites_available],node[:nginx][:balancing_conf]].join('/')
+default_conf 			    = [node[:nginx][:conf_dir],node[:nginx][:sites_available],node[:nginx][:balancing_conf]].join('/')
 default_conf_enabled	= [node[:nginx][:conf_dir],node[:nginx][:sites_enabled],node[:nginx][:balancing_conf]].join('/')
 
 nodes = []
@@ -40,3 +40,49 @@ link default_conf_enabled do
   only_if "test -f #{default_conf}"
   notifies :restart, "service[nginx]"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# partial_search(:node, 'recipes:white-app',
+#   :keys => { 'name' => [ 'name' ],
+#              'ip'   => [ 'ipaddress' ],
+#              'kernel_version' => [ 'kernel', 'version' ]
+#            }
+# ).each do |result|
+#   if result['name'] == 'front-1'
+#     nodes.push({name: 'front-1', ip: '192.168.56.102'})
+#   elsif result['name'] == 'front-2'
+#   nodes.push({name: 'front-2', ip: '192.168.56.103'})
+#   end
+#   # nodes.push({name: result['name'], ip: result['ip']})
+#   # or
+#   # nodes.push(result)
+# end
+
+# template default_conf do
+#   source "lb.conf.erb"
+#   variables(
+#     :nodes => nodes,
+#     :location => '/',
+#     :pool => 'apppool',
+#     :application => 'whiteapp',
+#     :version => node['white-app'][:version]
+#   )
+#   mode "0644"
+#   action :create
+#   notifies :restart, "service[nginx]"
+# end
+
--- cookbooks/white-app/recipes/mep.rb
+++ cookbooks/white-app/recipes/mep.rb
@@ -9,39 +9,83 @@
 
 # puts node['white-app'][:version]
 
-if node['white-app'][:current_version] != node['white-app'][:version]
-	git [node[:project][:home], node[:project][:name]].join('/') do
-		repository node[:git][:repository]
-		revision node['white-app'][:version]
-		# user, ssh_wrapper, timeout
-		action :sync
-	end
 
-	execute "npm install" do
-		command "npm install"
-		cwd		[node[:project][:home], node[:project][:name]].join('/')
-	end
 
-	execute "bower install" do
-		command "bower install --allow-root"
-		cwd		[node[:project][:home], node[:project][:name]].join('/')
-	end
 
-	execute "grunt build" do
-		command "grunt build"
-		cwd		[node[:project][:home], node[:project][:name]].join('/')
-	end
 
-	directory [node[:apache][:home_dir], node[:project][:name], node['white-app'][:version]].join('/') do
-		mode 0755
-		recursive true
-		action :create
-	end
 
-	execute "copy app" do
-		command "cp -r #{[node[:project][:home], node[:project][:name], 'dist', '*'].join('/')} #{[node[:apache][:home_dir], node[:project][:name], node['white-app'][:version]].join('/')}"
-	end
 
-	# Set the current version to deployed application
-	node.default['white-app'][:current_version] = node['white-app'][:version]
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if node['white-app'][:current_version] != node['white-app'][:version]
+# 	git [node[:project][:home], node[:project][:name]].join('/') do
+# 		repository node[:git][:repository]
+# 		revision node['white-app'][:version]
+# 		# user, ssh_wrapper, timeout
+# 		action :sync
+# 	end
+
+# 	execute "npm install" do
+# 		command "npm install"
+# 		cwd		[node[:project][:home], node[:project][:name]].join('/')
+# 	end
+
+# 	execute "bower install" do
+# 		command "bower install --allow-root"
+# 		cwd		[node[:project][:home], node[:project][:name]].join('/')
+# 	end
+
+# 	execute "grunt build" do
+# 		command "grunt build"
+# 		cwd		[node[:project][:home], node[:project][:name]].join('/')
+# 	end
+
+# 	directory [node[:apache][:home_dir], node[:project][:name], node['white-app'][:version]].join('/') do
+# 		mode 0755
+# 		recursive true
+# 		action :create
+# 	end
+
+# 	execute "copy app" do
+# 		command "cp -r #{[node[:project][:home], node[:project][:name], 'dist', '*'].join('/')} #{[node[:apache][:home_dir], node[:project][:name], node['white-app'][:version]].join('/')}"
+# 	end
+
+# 	# Set the current version to deployed application
+# 	node.default['white-app'][:current_version] = node['white-app'][:version]
+# end
